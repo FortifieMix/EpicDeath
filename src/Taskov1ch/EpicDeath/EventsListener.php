06b2f465f38f083faa2f175c061d9ea9c0c53aa1
Taskov1ch <tynaev.acc@gmail.com>
2024-11-23 16:14:02 +0600

    It seems to work

--- src/Taskov1ch/EpicDeath/EventsListener.php
+++ src/Taskov1ch/EpicDeath/EventsListener.php
@@ -2,11 +2,13 @@
 
 namespace Taskov1ch\EpicDeath;
 
+use pocketmine\entity\Entity;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\server\CommandEvent;
 use pocketmine\player\Player;
 use Taskov1ch\EpicDeath\events\EpicDeathEvent;
@@ -17,60 +19,74 @@ class EventsListener implements Listener
 
 	public function onDamage(EntityDamageEvent $event): void
 	{
-		$victim = $event->getEntity();
-
-		if ($this->main->inProcess($victim)) {
-			return;
-		}
-
 		if (!$event instanceof EntityDamageByEntityEvent) {
 			return;
 		}
 
-		if ($this->main->inProcess($event->getDamager())) {
+		$victim = $event->getEntity();
+		$damager = $event->getDamager();
+
+		if ($this->isInProcess($victim) or $this->isInProcess($damager)) {
+			$event->cancel();
 			return;
 		}
 
-		$health = $victim->getHealth();
-
-		if ($health > $event->getFinalDamage()) {
+		if (!$victim instanceof Player or !$victim->hasPermission("epic.death.use") or $this->main->inCd($victim)) {
 			return;
 		}
 
-		$event->cancel();
+		if ($victim->getHealth() > $event->getFinalDamage() or $victim === $damager) {
+			return;
+		}
+
 		$customEvent = new EpicDeathEvent($victim);
 		$customEvent->call();
 
 		if ($customEvent->isCancelled()) {
-			$event->uncancel();
 			return;
 		}
 
+		$event->setBaseDamage(0);
 		$this->main->startProcess($victim);
+	}
+
+	public function onDrop(PlayerDropItemEvent $event): void
+	{
+		$this->cancelIfInProcess($event->getPlayer(), $event);
 	}
 
 	public function onInteract(PlayerInteractEvent $event): void
 	{
-		if ($this->main->inProcess($event->getPlayer())) {
-			$event->cancel();
-		}
+		$this->cancelIfInProcess($event->getPlayer(), $event);
 	}
 
 	public function onCommand(CommandEvent $event): void
 	{
 		$sender = $event->getSender();
+		if ($sender instanceof Player) {
+			$this->cancelIfInProcess($sender, $event);
+		}
+	}
 
-		if ($sender instanceof Player and $this->main->inProcess($sender)) {
+	public function onMove(PlayerMoveEvent $event): void
+	{
+		$player = $event->getPlayer();
+		$distance = $event->getFrom()->distanceSquared($event->getTo());
+
+		if ($this->main->inProcess($player) && $distance >= 0.01) {
 			$event->cancel();
 		}
 	}
 
-	public function onRespawn(PlayerRespawnEvent $event): void
+	private function cancelIfInProcess(Player $player, $event): void
 	{
-		$player = $event->getPlayer();
-
 		if ($this->main->inProcess($player)) {
-			$this->main->unsetProcess($player);
+			$event->cancel();
 		}
+	}
+
+	private function isInProcess(Entity $entity): bool
+	{
+		return $entity instanceof Player && $this->main->inProcess($entity);
 	}
 }
--- src/Taskov1ch/EpicDeath/Main.php
+++ src/Taskov1ch/EpicDeath/Main.php
@@ -9,12 +9,13 @@ use pocketmine\player\GameMode;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
-use pocketmine\world\particle\SmokeParticle;
+use pocketmine\world\particle\EnchantmentTableParticle;
+use pocketmine\world\particle\MobSpawnParticle;
 
 class Main extends PluginBase
 {
-	public array $cooldowns = [];
-	public array $process = [];
+	private array $process = [];
+	private array $cd = [];
 	private array $config;
 
 	protected function onEnable(): void
@@ -25,70 +26,86 @@ class Main extends PluginBase
 
 	protected function onDisable(): void
 	{
-		foreach ($this->process as $player) {
-			$this->unsetProcess($player);
-			$player->kill();
-		}
+		array_map(fn(Player $player): void => $player->kill(), $this->process);
+		$this->process = [];
 	}
 
 	private function sendSound(array $sounds, Player $player): void
 	{
 		$pos = $player->getPosition();
+		$worldPlayers = $player->getWorld()->getPlayers();
 
-		foreach ($sounds as $sound) {
+		array_map(function ($sound) use ($pos, $worldPlayers): void {
 			$pk = PlaySoundPacket::create(
 				$sound, $pos->getX(), $pos->getY(), $pos->getZ(),
 				$this->config["volume"], $this->config["pitch"]
 			);
+			array_map(fn(Player $p): bool => $p->getNetworkSession()->sendDataPacket(clone $pk), $worldPlayers);
+		}, $sounds);
+	}
 
-			foreach ($player->getWorld()->getPlayers() as $player) {
-				$player->getNetworkSession()->sendDataPacket(clone $pk);
-			}
-		}
+	public function inCd(Player $player): bool
+	{
+		return time() <= ($this->cd[strtolower($player->getName())] ?? 0);
 	}
 
 	public function inProcess(Player $player): bool
 	{
-		return in_array($player, $this->process);
-	}
-
-	public function unsetProcess(Player $player): void
-	{
-		$player->setNoClientPredictions(false);
-		unset($this->process[array_search($player, $this->process)]);
+		return in_array($player, $this->process, true);
 	}
 
 	public function startProcess(Player $player): void
 	{
-		if ($this->inProcess($player)) {
-			return;
-		}
+		if ($this->inProcess($player)) return;
 
 		$this->process[] = $player;
+		$tmpGm = $player->getGamemode();
 		$player->setGamemode(GameMode::ADVENTURE());
-		$player->setNoClientPredictions(true);
 		$this->sendSound($this->config["start"], $player);
+
 		$player->getEffects()->add(new EffectInstance(
 			VanillaEffects::LEVITATION(), $this->config["duration"] * 20 + 10, 0, false
 		));
 
 		$task = $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(
-			function () use ($player)
+			function () use ($player): void
 			{
-				$player->getWorld()->addParticle(
-					$player->getPosition(),
-					new SmokeParticle(1)
-				);
+				$pos = $player->getPosition();
+				$world = $player->getWorld();
+
+				for ($i = 0; $i < 10; $i++) {
+					$world->addParticle(
+						$pos->add(mt_rand(-5, 5) / 10, mt_rand(-5, 5) / 10, mt_rand(-5, 5) / 10),
+						new EnchantmentTableParticle()
+					);
+				}
 			}
 		), 10);
 
 		$this->getScheduler()->scheduleDelayedTask(new ClosureTask(
-			function () use ($player, $task)
-			{
+			function () use ($player, $task, $tmpGm): void {
 				$task->remove();
-				$this->sendSound($this->config["end"], $player);
-				$player->kill();
+				$this->endProcess($player, $tmpGm);
 			}
 		), $this->config["duration"] * 20);
+	}
+
+	private function endProcess(Player $player, GameMode $tmpGm): void
+	{
+		$player->setGamemode($tmpGm);
+		$this->sendSound($this->config["end"], $player);
+
+		for ($i = 0; $i < 10; $i++) {
+			$player->getWorld()->addParticle($player->getPosition(), new MobSpawnParticle(3, 3));
+		}
+
+		$player->kill();
+		$this->removeFromProcess($player);
+		$this->cd[strtolower($player->getName())] = time() + $this->config["cd"];
+	}
+
+	private function removeFromProcess(Player $player): void
+	{
+		$this->process = array_filter($this->process, fn(Player $p): bool => $p !== $player);
 	}
 }
